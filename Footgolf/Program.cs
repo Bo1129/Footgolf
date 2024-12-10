6d46ed147ffee3f32a753d049522b26c83408153
Bo1129 <bohmb.22d@acsjszki.hu>
2024-12-10 12:45:10 +0100

    Add project files.

---
+++ Footgolf/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Footgolf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Versenyzo> versenyzok = new();
+
+            foreach (var sor in File.ReadAllLines("fob2016.txt"))
+            {
+                versenyzok.Add(new Versenyzo(sor));
+            }
+            //3.feladat
+         
+                Console.WriteLine($"3. feladat: Versenyzok száma: {versenyzok.Count}");
+            //4.feladat
+            int versenyzok_szama = versenyzok.Count;
+            int noi = 0;
+            foreach (var v in versenyzok)
+            {
+                if (v.Kategoria == "Noi")
+                {
+                    noi++;
+                }
+            }
+
+            Console.WriteLine($"A női versenyzők aránya: {versenyzok_szama/100*noi}%");
+
+        }
+    }
+}
---
+++ Footgolf/Versenyzo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Footgolf
+{
+    internal class Versenyzo
+    {
+        public string Nev { get; set; }
+        public string Kategoria { get; set; }
+        public string Egyesulet { get; set; }
+        public int[] Fordulok { get; set; }
+
+        public Versenyzo(string sor)
+        {
+            string[] s = sor.Split(';');
+
+            Nev = s[0];
+            Kategoria = s[1];
+            Egyesulet = s[2];
+            Fordulok = new int[8];
+            Fordulok[0] = int.Parse(s[3]);
+            Fordulok[1] = int.Parse(s[4]);
+            Fordulok[2] = int.Parse(s[5]);
+            Fordulok[3] = int.Parse(s[6]);
+            Fordulok[4] = int.Parse(s[7]);
+            Fordulok[5] = int.Parse(s[8]);
+            Fordulok[6] = int.Parse(s[9]);
+            Fordulok[7] = int.Parse(s[10]);
+        }
+
+
+    }
+}
